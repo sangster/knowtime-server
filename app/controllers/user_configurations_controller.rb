de4b49549fae193d2d002518c72c4e7e6585f265
Jon Sangster <jon@ertt.ca>
2013-11-24 03:29:15 -0400

    /check_remote_zip/

--- app/controllers/user_configurations_controller.rb
+++ app/controllers/user_configurations_controller.rb
@@ -2,4 +2,11 @@ class UserConfigurationsController < ApplicationController
   def pollrate
     @pollrate = 3
   end
+
+  def check_remote_zip
+    unless ENV['DISABLE_INITIALIZER_FROM_RAKE']
+      Rails.cache.clear
+      DataPull.check_remote_zip
+    end
+  end
 end

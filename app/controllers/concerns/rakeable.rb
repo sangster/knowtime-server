f47cb40179439b97b9d54ad5d4633b2251811cf7
Jon Sangster <jon@ertt.ca>
2013-11-24 19:07:47 -0400

    rake log file

--- app/controllers/concerns/rakeable.rb
+++ app/controllers/concerns/rakeable.rb
@@ -4,6 +4,6 @@ module Rakeable
   def call_rake(task, options={})
     options[:rails_env] = Rails.env
     args = options.collect { |n, v| "#{n.to_s.upcase}='#{v}'" }
-    system "env rake #{task} #{args.join ' '} &"
+    system "env rake #{task} #{args.join ' '} >> #{Rails.root}/log/rake.log &"
   end
 end

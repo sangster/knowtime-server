adb53ba8f5a3c9f2526f9558572422a654ad4fa2
Jon Sangster <jon@ertt.ca>
2013-11-24 20:12:56 -0400

    logging

--- app/controllers/concerns/rakeable.rb
+++ app/controllers/concerns/rakeable.rb
@@ -4,6 +4,9 @@ module Rakeable
   def call_rake(task, options={})
     options[:rails_env] = Rails.env
     args = options.collect { |n, v| "#{n.to_s.upcase}='#{v}'" }
-    system "rake #{task} #{args.join ' '} >> #{Rails.root}/log/rake.log &"
+    cmd = "rake #{task} #{args.join ' '} >> #{Rails.root}/log/rake.log &"
+
+    logger.info "Invoking rake task: #{cmd}"
+    system cmd
   end
 end

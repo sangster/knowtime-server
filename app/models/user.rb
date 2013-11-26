9d2a2942f7b5f865426cc900b152b042445ba65c
Jon Sangster <jon@ertt.ca>
2013-11-25 20:26:54 -0400

    Use scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ require 'ostruct'
 class User < ActiveRecord::Base
   has_many :user_locations, inverse_of: :user
 
+  scope :newest_locations, -> { where 'created_at > ?', (DateTime.now - RECENT_ENTRIES_TIME) }
+
   IS_MOVING_DELTA = 10 # metres
   AVERAGE_OLDER_WEIGHT = 0.25
   RECENT_ENTRIES_TIME = 30.seconds
@@ -20,11 +22,11 @@ class User < ActiveRecord::Base
   end
 
   def is_moving?
-    locs = user_locations
+    locs = self.newest_locations
     return false if locs.length < 2
 
     first = locs.first
-    first_distant_location = locs[1..-1].find { |loc| not loc.nil? and loc.distance_from(first) > IS_MOVING_DELTA }
+    first_distant_location = locs[1..-1].find { |loc| loc.distance_from(first) > IS_MOVING_DELTA }
 
     not first_distant_location.nil?
   end
@@ -36,15 +38,14 @@ class User < ActiveRecord::Base
   private
 
   def calculate_average_location
-    newest_locations = user_locations.where 'created_at > ?', (DateTime.now - RECENT_ENTRIES_TIME)
-    if newest_locations.empty?
+    if self.newest_locations.empty?
       nil
     else
-      first = newest_locations.first
+      first = self.newest_locations.first
       lat = first.lat
       lng = first.lng
 
-      newest_locations[1..-1].each do |loc|
+      self.newest_locations[1..-1].each do |loc|
         lat = lat + AVERAGE_OLDER_WEIGHT * (loc.lat - lat)
         lng = lng + AVERAGE_OLDER_WEIGHT * (loc.lng - lng)
       end

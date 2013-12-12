2ba954865a0b7b27702afd74bd979b223a030903
Jon Sangster <jon@ertt.ca>
2013-12-11 23:27:10 -0400

    fix spec messages

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,27 +4,27 @@ describe User do
   subject { create :user }
 
   context 'created programatically' do
-    it 'to have a short name' do
+    it 'should have a short name' do
       expect(subject.short_name.length).to be > 0
     end
 
-    it 'to not be moving' do
+    it 'should not be moving' do
       expect(subject.moving?).to be false
     end
 
-    it 'to have a uuid' do
+    it 'should have a uuid' do
       expect(subject.uuid_str).to match /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/
     end
 
-    it 'to only create uuid_str once' do
+    it 'should only create uuid_str once' do
       expect(subject.uuid_str).to be subject.uuid_str
     end
 
-    it 'to have an empty list of user locations' do
+    it 'should have an empty list of user locations' do
       expect(subject.user_locations).to be_a_kind_of Array
     end
 
-    it 'to have no newest location' do
+    it 'should have no newest location' do
       expect(subject.newest_location).to be_nil
     end
 
@@ -35,7 +35,7 @@ describe User do
     context 'with user locations' do
       subject { create :user_with_locations }
 
-      it 'to have an newest location' do
+      it 'should have an newest location' do
         expect(subject.newest_location).to be_a_kind_of UserLocation
       end
 
@@ -44,7 +44,7 @@ describe User do
       context 'and is moving' do
         before { subject.moving? }
 
-        it 'moving state persisted' do
+        it 'should have moving state persisted' do
           expect(subject.moving).to be true
         end
       end
@@ -68,7 +68,7 @@ describe User do
 
         it { expect(subject.moving).to be false }
 
-        it 'to have an newest location' do
+        it 'should have an newest location' do
           expect(subject.newest_location).to be_a_kind_of UserLocation
         end
       end
@@ -76,7 +76,7 @@ describe User do
   end
 
   context 'from db' do
-    it 'can be fetched with uuid_str' do
+    it 'should be fetched with uuid_str' do
       expect(User.get(subject.uuid_str).uuid_str).to eq subject.uuid_str
     end
   end

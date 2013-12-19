68eb7da69d8a48089faf5b1590dc451be1fbb0a4
Jon Sangster <jon@ertt.ca>
2013-12-18 22:18:43 -0400

    specs for Calendar

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -17,41 +17,45 @@ class Calendar
   embeds_many :calendar_exceptions
   has_many :trips
 
-  def self.new_from_csv(row)
-    {_id: row[:service_id],
-     start_date: to_date(row[:start_date]),
-     end_date: to_date(row[:end_date]),
-     weekday: to_bool(row[:monday]),
-     saturday: to_bool(row[:saturday]),
-     sunday: to_bool(row[:sunday])}
-   end
+  class << self
+    def new_from_csv(row)
+      {_id: row[:service_id],
+       start_date: to_date(row[:start_date]),
+       end_date: to_date(row[:end_date]),
+       weekday: to_bool(row[:monday]),
+       saturday: to_bool(row[:saturday]),
+       sunday: to_bool(row[:sunday])}
+     end
 
-   def self.to_date(str)
-    Date.strptime str, '%Y%m%d'
-  end
+     def for_date_params(params)
+      for_date Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+    end
 
-  def self.to_bool(str)
-    return str == '1'
-  end
+    def for_date(date)
+      criteria = Calendar.where(:start_date.lte => date).where(:end_date.gte => date)
+      if date.saturday?
+        criteria.where saturday: true
+      elsif date.sunday?
+        criteria.where sunday: true
+      else
+        criteria.where weekday: true
+      end.to_a
+    end
 
-  def self.for_date_params(params)
-    for_date Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-  end
+    def for_uuid(uuid_str)
+      key = Uuid.key_for uuid_str
+      Calendar.find key unless key.nil?
+    end
 
-  def self.for_date(date)
-    criteria = Calendar.where(:start_date.lte => date).where(:end_date.gte => date)
-    if date.saturday?
-      criteria.where saturday: true
-    elsif date.sunday?
-      criteria.where sunday: true
-    else
-      criteria.where weekday: true
-    end.to_a
-  end
+    private
 
-  def self.for_uuid(uuid_str)
-    key = Uuid.key_for uuid_str
-    Calendar.find key unless key.nil?
+    def to_date(str)
+      Date.strptime str, '%Y%m%d'
+    end
+
+    def to_bool(str)
+      return str == '1'
+    end
   end
 
   def uuid

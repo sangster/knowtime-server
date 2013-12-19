28fe7da3996ade3a22aeca6fe51eff3f691ea15e
Jon Sangster <jon@ertt.ca>
2013-12-18 22:31:34 -0400

    extract code from controller

--- app/controllers/estimations_controller.rb
+++ app/controllers/estimations_controller.rb
@@ -2,60 +2,7 @@ class EstimationsController < ApplicationController
   def index_for_short_name
     now = Time.zone.at( DateTime.now ).time
     short_name = params[:short_name]
-
-    next_stops = StopTime.next_stops short_name, now, 30.minutes
-
-    users = User.where short_name: short_name
-    groups = UserGroup.create_groups users
-
-
-    @estimations = map_estimates groups, next_stops
-  end
-
-
-  private
-
-
-  def map_estimates(groups, next_stops)
-    return [] if next_stops.empty?
-
-    estimates = []
-    options = next_stops
-
-    groups.select(&:location).each do |group|
-      group_loc = group.location
-
-      closest_index = 0
-      closest_dist = Distanceable::EARTH_DIAMETER
-
-      options.each_index do |i|
-        stop_loc = options[i].stop.location
-        dist = stop_loc.distance_from group_loc
-        if dist < closest_dist
-          closest_index = i
-          closest_dist = dist
-        end
-      end
-
-      opt = options.delete_at closest_index
-      estimates << BusEstimation.new(opt.stop_number, opt.arrival, group_loc.lat, group_loc.lng)
-    end
-
-    options.each do |opt|
-      estimates << BusEstimation.new(opt.stop_number, opt.arrival, 0, 0)
-    end
-
-    stop_numbers_seen = []
-    estimates.each do |est|
-      stop_number = est.stop_number
-
-      if stop_numbers_seen.include? stop_number
-        estimates.delete est
-      else
-        stop_numbers_seen << stop_number
-      end
-    end
-
-    estimates.uniq! { |e| e.stop_number }
+    
+    @estimations = BusEstimation.locations_and_next_stops short_name, now, 30.minutes
   end
 end
--- app/models/bus_estimation.rb
+++ app/models/bus_estimation.rb
@@ -2,6 +2,64 @@ class BusEstimation
   include Distanceable
   attr_accessor :stop_number, :arrival, :lat, :lng
 
+  class << self
+    def locations_and_next_stops(short_name, time, duration = nil)
+      next_stops = StopTime.next_stops short_name, time, duration
+
+      users = User.where short_name: short_name
+      groups = UserGroup.create_groups users
+
+      map_estimates groups, next_stops
+    end
+
+
+    private
+
+
+    def map_estimates(groups, next_stops)
+      return [] if next_stops.empty?
+
+      estimates = []
+      options = next_stops
+
+      groups.select(&:location).each do |group|
+        group_loc = group.location
+
+        closest_index = 0
+        closest_dist = Distanceable::EARTH_DIAMETER
+
+        options.each_index do |i|
+          stop_loc = options[i].stop.location
+          dist = stop_loc.distance_from group_loc
+          if dist < closest_dist
+            closest_index = i
+            closest_dist = dist
+          end
+        end
+
+        opt = options.delete_at closest_index
+        estimates << BusEstimation.new(opt.stop_number, opt.arrival, group_loc.lat, group_loc.lng)
+      end
+
+      options.each do |opt|
+        estimates << BusEstimation.new(opt.stop_number, opt.arrival, 0, 0)
+      end
+
+      stop_numbers_seen = []
+      estimates.each do |est|
+        stop_number = est.stop_number
+
+        if stop_numbers_seen.include? stop_number
+          estimates.delete est
+        else
+          stop_numbers_seen << stop_number
+        end
+      end
+
+      estimates.uniq! { |e| e.stop_number }
+    end
+  end
+
   def initialize(stop_number, arrival, lat, lng)
     self.stop_number = stop_number
     self.arrival = arrival

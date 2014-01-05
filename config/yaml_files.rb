77084226633b4a28a141b9218de9703f8dc0cc15
Jon Sangster <jon@ertt.ca>
2014-01-04 23:28:47 -0400

    Heroku: read secrets from ENV instead of /etc file

--- config/yaml_files.rb
+++ config/yaml_files.rb
@@ -2,8 +2,14 @@ require 'yaml'
 
 METRO_TRANSIT = YAML.load_file("config/metro_transit.yml")[::Rails.env]
 
-if ::Rails.env == 'production'
-  SECRETS = YAML.load_file("/etc/knowtime_secrets.yml")
+SECRETS = if ::Rails.env == 'production'
+  if File.readable? '/etc/knowtime_secrets.yml'
+    YAML.load_file('/etc/knowtime_secrets.yml')
+  else
+    { 'knowtime' => { 'admin_uuid' => ENV['knowtime-admin_uuid'] },
+      'rails'    => { 'key_base'   => ENV['rails-key_base'] },
+      'api_keys' => { 'new_relic'  => ENV['api_keys-new_relic'] } }
+  end
 else
-  SECRETS = YAML.load_file("config/development_secrets.yml")
+  YAML.load_file('config/development_secrets.yml')
 end

7b30a6616eed484fd82c095602a06bdb003c0a2e
Jon Sangster <jon@ertt.ca>
2014-01-24 03:01:28 -0400

    Change Calendars#table to Schedule#show

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -4,17 +4,7 @@ class CalendarsController < ApplicationController
     render_error :not_found, "no calendar found for UUID: #{params[:calendar_uuid]}" if @calendar.nil?
   end
 
-
   def index_for_date
     @calendars = Calendar.for_date_params params
-  end
-
-  def table
-    cal = Calendar.for_date( time_from_params ).first
-    @tables = {}
-
-    cal.trip_groups( params[:short_name] ).each_pair do |heading,trips|
-      @tables[heading] = CalendarTable.new heading, trips
-    end
   end
 end

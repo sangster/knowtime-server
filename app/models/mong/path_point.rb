d59e7ab2206ef5da6e275565c1642e60b3e40b11
Jon Sangster <jon@ertt.ca>
2014-03-13 21:10:47 -0300

    Remove old mongoid models

--- app/models/mong/path_point.rb
+++
@@ -1,9 +0,0 @@
-class PathPoint
-  include Mongoid::Document
-
-  embedded_in :path, inverse_of: :path_points
-
-  field :i, as: :index, type: Integer
-  field :t, as: :lat, type: Float
-  field :g, as: :lng, type: Float
-end

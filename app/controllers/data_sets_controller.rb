6574241c3b6ed26be63015413610531c4cfdb117
Jon Sangster <jon@ertt.ca>
2014-07-13 15:03:53 -0300

    cache gtfs summaries

--- app/controllers/data_sets_controller.rb
+++ app/controllers/data_sets_controller.rb
@@ -3,8 +3,12 @@ class DataSetsController < ActionController::Base
   respond_to :json
 
   def index
-    @data_sets = GtfsEngine::DataSet.all.collect do |data_set|
-      SummarizeDataSetContext.new.set_data(data_set).call
+    expires_in 3.hours
+
+    @data_sets = Rails.cache.fetch "data_set_summaries" do
+      GtfsEngine::DataSet.all.collect do |data_set|
+        SummarizeDataSetContext.new.set_data(data_set).call
+      end
     end
 
     respond_with @data_sets

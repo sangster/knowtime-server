333b837dff0678657f7de7e7a8f2e6358a5b87b4
Jon Sangster <jon@ertt.ca>
2014-07-17 21:58:50 -0300

    Reorganize data sets JSON

--- app/controllers/data_sets_controller.rb
+++ app/controllers/data_sets_controller.rb
@@ -1,13 +1,31 @@
+# This file is part of the KNOWtime server.
+#
+# The KNOWtime server is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# The KNOWtime server is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the KNOWtime server.  If not, see <http://www.gnu.org/licenses/>.
 class DataSetsController < ApplicationController
   def index
     expires_in 3.hours
 
     @data_sets = Rails.cache.fetch "data_set_summaries" do
-      GtfsEngine::DataSet.all.collect do |data_set|
+      sets = {}
+      GtfsEngine::DataSet.order(:created_at).collect do |data_set|
         SummarizeDataSetContext.new \
           .set_data(data_set) \
           .call
+      end.each do |data_set|
+        sets[data_set.name] = data_set
       end
+      sets.values
     end
 
     respond_with @data_sets
--- config/initializers/gtfs_sources.rb
+++ config/initializers/gtfs_sources.rb
@@ -1,9 +1,27 @@
+# This file is part of the KNOWtime server.
+#
+# The KNOWtime server is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# The KNOWtime server is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the KNOWtime server.  If not, see <http://www.gnu.org/licenses/>.
 GtfsEngine.send_aliased_keys = true
 
 GtfsEngine.sources do |s|
+  s.halifax_localhost do
+    title 'Halifax Metro (test)'
+    url METRO_TRANSIT['zip_url']
+  end
   s.halifax do
     title 'Halifax Metro'
-    url METRO_TRANSIT['zip_url']
+    url 'http://www.halifax.ca/metrotransit/googletransitfeed/google_transit.zip'
   end
   s.edmonton do
     title "Edmonton Transit System"
